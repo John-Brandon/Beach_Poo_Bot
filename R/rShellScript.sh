b87d44eef5930379f354b6c6f8d830d3a67347c6
John Brandon <jbrandon@gmail.com>
2017-01-10 17:18:26 -0800

    first commit

---
+++ R/rShellScript.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+#
+# Bash shell script to run R code
+cd ~/Documents/R_hobby/BeachWater
+/Library/Frameworks/R.framework/Versions/3.2/Resources/bin/Rscript ~/Documents/R_hobby/BeachWater/R/TweetPoo.R
