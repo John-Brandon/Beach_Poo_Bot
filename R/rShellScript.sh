790e50e59376a16fdc05e33f54ed7b432a202eed
John Brandon <jbrandon@gmail.com>
2017-01-20 19:14:34 -0800

    updated to R version 3.3

--- R/rShellScript.sh
+++ R/rShellScript.sh
@@ -1,5 +1,14 @@
-#!/bin/bash
+#!/bin/bash -v
 # 
 # Bash shell script to run R code:
 cd ~/Documents/R_hobby/BeachWater  # Edit paths as appropriate to your machine
-/Library/Frameworks/R.framework/Versions/3.2/Resources/bin/Rscript ~/Documents/R_hobby/BeachWater/R/TweetShit.R
+
+# Old version of R. 
+#/Library/Frameworks/R.framework/Versions/3.2/Resources/bin/Rscript ~/Documents/R_hobby/BeachWater/R/TweetShit.R
+
+# Current version of R. 
+/Library/Frameworks/R.framework/Versions/3.3/Resources/bin/Rscript ~/Documents/R_hobby/BeachWater/R/TweetShit.R
+
+# Try with just basename Rscript. Works fine from command line. 
+#Rscript ~/Documents/R_hobby/BeachWater/R/TweetShit.R
+
